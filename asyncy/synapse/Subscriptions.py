6ff0f3553a54ea1e4ccb053b2a5ca0f8462b78b2
Jude Pereira <contact@judepereira.com>
2018-11-29 17:16:09 +0530

    Use a single Kubernetes watch call for all Pods.
    When creating multiple watches for each subscription, a subscription
    count greater than the number of CPUs lead to the blocking of the
    event loop in asyncio - probably due to the k8s asyncio library
    not handling this correctly.

--- asyncy/synapse/Subscriptions.py
+++ asyncy/synapse/Subscriptions.py
@@ -3,6 +3,7 @@ import json
 from tornado.httpclient import AsyncHTTPClient
 
 from .Entities import Subscription
+from .Exceptions import NotFoundException
 from .helpers.HttpHelper import HttpHelper
 from .DB import DB
 
@@ -47,7 +48,12 @@ class Subscriptions:
         Check if the container ID has actually changed against the DB,
         and if it has, resubscribe with retry.
         """
-        s = await DB.get_subscription(sub_id)
+        try:
+            s = await DB.get_subscription(sub_id)
+        except NotFoundException:
+            logger.warning(f'Did not find a subscription for {sub_id}!')
+            raise
+
         if s.container_id != container_id:
             logger.debug(f'Re-subscribing {sub_id}...')
             await cls._subscribe(s.url, s.method.upper(), s.payload)
--- asyncy/synapse/handlers/UnsubscribeHandler.py
+++ asyncy/synapse/handlers/UnsubscribeHandler.py
@@ -12,6 +12,7 @@ class UnsubscribeHandler(tornado.web.RequestHandler):
     async def post(self):
         payload = json.loads(self.request.body)
         app_id = payload['app_id']
+        pod_name = payload['pod_name']
         sub_id = payload['sub_id']
-        await Kubernetes.remove_watch(app_id, sub_id)
+        await Kubernetes.remove_watch(app_id, pod_name, sub_id)
         await DB.delete_one_subscription(app_id, sub_id)
